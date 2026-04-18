91003eb5882773264a79bb10e8c3b3c9ec08979d
Arjin Joshi <thearjinjoshi1@gmail.com>
2026-04-18 16:18:56 +0545

    [Typed package install and update Homepage]

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,20 +1,42 @@
 
+import { useEffect, useRef } from "react";
 import { MdMail } from "react-icons/md";
 import { useNavigate } from "react-router"
+import Typed from "typed.js";
 
 
 const Homepage = () => {
 
   const navigate = useNavigate();
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "Full Stack Developer",
+        "Web Developer",
+        "Frontend Developer",
+        "Backend Developer",
+        "Coder",
+      ],
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 1000,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
     <div className='flex justify-between h-full mt-30 items-center'>
         <div className='text-lightdarkblue tracking-wide flex flex-col gap-5'>
             <h1 className='text-4xl'>Hi! Arjin Joshi </h1>
             <div className='text-4xl'>
-                I am a 
-                <span className='text-lightpurple'> Full Stack Developer
-                </span>
+              I am a{" "}
+              <span ref={el} className='text-orange-500'></span>
             </div>
 
         <p className='text-lg tracking-wide'>I'm a software developer and here is my portfolio website. <br /> Here you'll learn about my journey as a software developer.</p>
